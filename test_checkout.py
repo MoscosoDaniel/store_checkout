d04ba8325e63f491698f7388e3ce4d7d584d4311
Daniel Moscoso <moscoso.daniel91@gmail.com>
2024-07-31 13:35:04 -0500

    Simplest implementation to make the unit pass.

--- test_checkout.py
+++ test_checkout.py
@@ -4,3 +4,8 @@ from checkout import Checkout
 def test_can_add_item_price():
     check_out = Checkout()
     check_out.add_item_price("a", 1)
+
+
+def test_can_add_item():
+    check_out = Checkout()
+    check_out.add_item("a")
