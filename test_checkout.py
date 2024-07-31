6b772edb2a7337fb7ad00774936f232f93b61887
Daniel Moscoso <moscoso.daniel91@gmail.com>
2024-07-31 14:14:22 -0500

    Refactor phase: the first 2 tests are not needed anymore, since they are implemented in the 'test_calculate_total'.

--- test_checkout.py
+++ test_checkout.py
@@ -8,14 +8,6 @@ def checkout():
     return check_out
 
 
-def test_can_add_item_price(checkout):
-    checkout.add_item_price("a", 1)
-
-
-def test_can_add_item(checkout):
-    checkout.add_item("a")
-
-
 def test_can_calculate_total(checkout):
     checkout.add_item_price("a", 1)
     checkout.add_item("a")
