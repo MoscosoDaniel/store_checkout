f5bb663cbc9f6b40645bc37905b9e14ccc525e0a
Daniel Moscoso <moscoso.daniel91@gmail.com>
2024-07-31 13:11:29 -0500

    Refactor phase: 1st test is no longer needed.

--- test_checkout.py
+++ test_checkout.py
@@ -1,11 +1,6 @@
 from checkout import Checkout
 
 
-def test_can_instantiate_checkout():
-    check_out = Checkout()
-    return check_out
-
-
 def test_can_add_item_price():
     check_out = Checkout()
     check_out.add_item_price("a", 1)
